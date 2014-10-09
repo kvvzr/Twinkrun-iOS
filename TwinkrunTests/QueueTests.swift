408848d318b28932f7b3e97c03a7b7f66a76733f
kvvzr <kwzr@twinkrun.net>
2014-10-09 10:38:20 +0900

    add queue

---
+++ TwinkrunTests/QueueTests.swift
@@ -0,0 +1,46 @@
+//
+//  QueueTests.swift
+//  Twinkrun
+//
+//  Created by Kawazure on 2014/10/09.
+//  Copyright (c) 2014年 Twinkrun. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class QueueTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testQueue() {
+        var queue = [1]
+        queue.enqueue(2)
+        XCTAssertEqual(queue, [1, 2])
+        
+        var one = queue.dequeue()!
+        XCTAssertEqual(one, 1)
+        
+        queue.dequeue()
+        XCTAssertEqual(queue, [])
+        
+        queue.dequeue()
+        XCTAssertEqual(queue, [])
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
