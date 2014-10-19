467e7e00462fd76359cb5d2bf53e96c793424149
mactkg <mactkg@gmail.com>
2014-10-20 02:03:17 +0900

    better tests

--- TwinkrunTests/TWRPlayerTest.swift
+++ TwinkrunTests/TWRPlayerTest.swift
@@ -9,6 +9,7 @@
 import UIKit
 import XCTest
 import Twinkrun
+import CoreBluetooth
 
 class TWRPlayerTest: XCTestCase {
 
@@ -23,21 +24,38 @@ class TWRPlayerTest: XCTestCase {
     }
 
     func testPlayer() {
-        var option = TWROption.sharedInstance.gameOption
-        var player = TWRPlayer(playerName: "mactkg", identifier: nil, colorSeed:1024)
+        func test(player:TWRPlayer, cbUUID:CBUUID) -> Bool {
+            var option = TWROption.sharedInstance.gameOption
+            XCTAssertNotNil(player, "playerある")
+            XCTAssertTrue(player.colorSeed < 1024, "colorSeedは1024未満")
+           
+            XCTAssertNotNil(player.currentRole(0), "ちゃんと現在の色が返ってくる")
+            XCTAssertNotNil(player.currentRole(16), "ちゃんと現在の色が返ってくる")
+            XCTAssertNotNil(player.currentRole(option.gameTime()), "ちゃんと現在の色が返ってくる")
         
-        XCTAssertNotNil(player, "playerある")
-        XCTAssertEqual(player.playerName, "mactkg", "指定した名前になってる")
-        XCTAssertTrue(player.colorSeed < 1024, "colorSeedは1024未満")
+            XCTAssertNotNil(player.nextRole(0), "ちゃんと次の色が返ってくる")
+            XCTAssert(player.nextRole(0) == player.currentRole(3), "ちゃんと次の色が返ってくる")
+            XCTAssert(player.nextRole(5) == player.currentRole(8), "ちゃんと次の色が返ってくる")
+            XCTAssertNil(player.nextRole(option.gameTime()-3), "次がおしまいの時はnilが返ってくる")
        
-        XCTAssertNotNil(player.currentRole(0), "ちゃんと現在の色が返ってくる")
-        XCTAssertNotNil(player.currentRole(16), "ちゃんと現在の色が返ってくる")
-        XCTAssertNotNil(player.currentRole(option.gameTime()), "ちゃんと現在の色が返ってくる")
-    
-        XCTAssertNotNil(player.nextRole(0), "ちゃんと次の色が返ってくる")
-        XCTAssert(player.nextRole(0) == player.currentRole(3), "ちゃんと次の色が返ってくる")
-        XCTAssert(player.nextRole(5) == player.currentRole(8), "ちゃんと次の色が返ってくる")
-        XCTAssertNil(player.nextRole(option.gameTime()-3), "次がおしまいの時はnilが返ってくる")
+            let advData = player.advertisementData(cbUUID)
+            let nameKey:String = advData[CBAdvertisementDataLocalNameKey] as NSString
+            let playerUUID = (advData[CBAdvertisementDataServiceUUIDsKey] as NSArray).firstObject as CBUUID
+            XCTAssert(nameKey == "\(player.playerName),\(player.colorSeed)", "正しい配信情報を返してくれる")
+            XCTAssert(playerUUID == cbUUID, "UUIDが一致する")
+            
+            return true
+        }
+        
+        let uuid = NSUUID(UUIDString: "eddbfbac-57af-11e4-a09f-7831c1d35942")
+        let cbUUID = CBUUID.UUIDWithString("0e15d2fc-57ac-11e4-a8ea-7831c1d35942")
+        var player = TWRPlayer(playerName: "mactkg", identifier: uuid, colorSeed:1024)
+        XCTAssert(test(player, cbUUID), "普通のプレイヤーでうまくいく")
+       
+        let advData = player.advertisementData(cbUUID)
+        let nameKey:String = advData[CBAdvertisementDataLocalNameKey] as NSString
+        var remotePlayer = TWRPlayer(advertisementData:["kCBAdvDataLocalName": nameKey], identifier: uuid)
+        XCTAssert(test(remotePlayer, cbUUID), "複製してもうまくいく")
     }
 
     func testPerformanceExample() {
