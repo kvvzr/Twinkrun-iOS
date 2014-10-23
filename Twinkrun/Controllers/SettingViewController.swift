376014e114fa9c50cb52916f3038977219cbd95b
kvvzr <kwzr@twinkrun.net>
2014-10-24 01:47:36 +0900

    implement setting view

---
+++ Twinkrun/Controllers/SettingViewController.swift
@@ -0,0 +1,154 @@
+//
+//  SettingViewController.swift
+//  Twinkrun
+//
+//  Created by Kawazure on 2014/10/23.
+//  Copyright (c) 2014年 Twinkrun. All rights reserved.
+//
+
+
+import UIKit
+import CoreBluetooth
+
+enum SettingType {
+    case Input(defaultValue: String, onChange: (String, TWROption) -> Void)
+    case Select(title: String, defaultValue: String, values: [String], onChange: (String, TWROption) -> Void)
+    case PushView(title: String, onSelect: UINavigationController -> Void)
+}
+
+class SettingViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
+    var option: TWROption
+    let settings: [String: [SettingType]]
+
+    required init(coder aDecoder: NSCoder) {
+        option = TWROption.sharedInstance
+        settings = [
+            "Player Name": [
+                SettingType.Input(defaultValue: option.playerName, onChange: { (text, option) in option.playerName = text })
+            ],
+            "Help": [
+                SettingType.PushView(title: "Introduction", { navigationController in
+                }),
+                SettingType.PushView(title: "License", { navigationController in
+                    navigationController.pushViewController(VTAcknowledgementsViewController(), animated: true)
+                })
+            ]
+        ]
+        
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController!.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundView = nil
+        tableView.backgroundColor = UIColor.twinkrunBlack()
+        tableView.tableFooterView = UIView(frame: CGRectZero)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return settings.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let key = settings.keys.array[section]
+        return settings[key]!.count
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 48
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return settings.keys.array[section]
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if cell.respondsToSelector(Selector("separatorInset")) {
+            cell.separatorInset = UIEdgeInsetsZero
+        }
+        if cell.respondsToSelector(Selector("layoutMargins")) {
+            cell.layoutMargins = UIEdgeInsetsZero
+        }
+        
+        var view = UIView()
+        view.backgroundColor = UIColor.blackColor()
+        cell.selectedBackgroundView = view
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if view is UITableViewHeaderFooterView {
+            var header = view as UITableViewHeaderFooterView
+            header.contentView.backgroundColor = UIColor.blackColor()
+            header.textLabel.textColor = UIColor.whiteColor()
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let key = settings.keys.array[indexPath.section]
+        let setting = settings[key]![indexPath.row]
+        
+        switch setting {
+        case .Input(defaultValue: let value, onChange: let onChange):
+            var cell = tableView.dequeueReusableCellWithIdentifier("inputCell") as UITableViewCell
+            
+            cell.backgroundColor = UIColor.twinkrunBlack()
+            cell.separatorInset = UIEdgeInsetsZero
+            
+            var field = cell.viewWithTag(1) as UITextField
+            field.attributedPlaceholder = NSAttributedString(
+                string: "Player Name",
+                attributes: [NSForegroundColorAttributeName: UIColor.whiteColor().colorWithAlphaComponent(0.5)]
+            )
+            field.tintColor = UIColor.twinkrunGreen()
+            field.textColor = UIColor.whiteColor()
+            field.text = value
+            
+            return cell
+        case .Select(title: let title, defaultValue: let value, values: let values, onChange: let onChange):
+            var cell = UITableViewCell()
+            return cell
+        case .PushView(title: let title, onSelect: let onSelect):
+            var cell = UITableViewCell()
+            
+            cell.backgroundColor = UIColor.twinkrunBlack()
+            cell.separatorInset = UIEdgeInsetsZero
+            cell.tintColor = UIColor.whiteColor()
+            cell.textLabel.textColor = UIColor.whiteColor()
+            cell.textLabel.text = title
+            
+            return cell
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let key = settings.keys.array[indexPath.section]
+        let setting = settings[key]![indexPath.row]
+        
+        switch setting {
+        case .Input(defaultValue: let value, onChange: let onChange):
+            break
+        case .Select(title: let title, defaultValue: let value, values: let values, onChange: let onChange):
+            break
+        case .PushView(title: let title, onSelect: let onSelect):
+            onSelect(navigationController!)
+            break
+        }
+    }
+}
